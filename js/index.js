18a0ed1ac7efc50705faf90f755ae2e68af7046c
Ow1Max <0w1.go.0w1@gmail.com>
2023-04-30 01:11:44 +0300

    feat: Add data for lower and upper case, make a check to add a class

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
-const KEYS_ENG = [
-  ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '← Backspace'],
-  ['Tab ⇄', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
-  ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'Enter ↵'],
-  ['⇧ Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', '⇧ Shift'],
-  ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
-];
+const KEYS_ENG = {
+  lowerCase: [
+    ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
+    ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
+  ],
+  upperCase: [
+    ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
+    ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
+  ],
+};
 
-const KEYS_RUS = [
-  ['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '← Backspace'],
-  ['Tab ⇄', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
-  ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'д', 'ж', 'э', 'Enter ↵'],
-  ['⇧ Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', '⇧ Shift'],
-  ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
-];
+const KEYS_RUS = {
+  lowerCase: [
+    ['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
+    ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'д', 'ж', 'э', 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
+  ],
+  upperCase: [
+    ['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
+    ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'д', 'ж', 'э', 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '←', '↓', '→', 'Ctrl'],
+  ],
+};
+
+const KEYS_CLASS = ['Backspace', 'Tab', 'Del', 'Caps Lock', 'Enter', 'Shift', 'Ctrl', 'Win', 'Alt'];
 
 const body = document.querySelector('body');
 
@@ -21,6 +41,35 @@ const createKey = (element, key) => {
   const elementKeyText = document.createElement('span');
 
   elementKey.classList.add('keyboard__key');
+
+  if (KEYS_CLASS.includes(key)) {
+    if (key === 'Caps Lock') {
+      elementKey.classList.add('keyboard__key_caps-lock');
+    } else {
+      elementKey.classList.add(`keyboard__key_${key.toLowerCase()}`);
+    }
+  }
+
+  if (key === '') {
+    elementKey.classList.add('keyboard__key_space');
+  }
+
+  if (key === '↑') {
+    elementKey.classList.add('keyboard__key_up');
+  }
+
+  if (key === '→') {
+    elementKey.classList.add('keyboard__key_right');
+  }
+
+  if (key === '↓') {
+    elementKey.classList.add('keyboard__key_down');
+  }
+
+  if (key === '←') {
+    elementKey.classList.add('keyboard__key_left');
+  }
+
   elementKeyText.textContent = key;
 
   element.insertAdjacentElement('beforeend', elementKey);
@@ -34,7 +83,7 @@ const createKeyboard = (lang) => {
   elementContainer.classList.add('container');
   elementKeyboard.classList.add('keyboard');
 
-  body.insertAdjacentElement('beforeend', elementContainer);
+  body.insertAdjacentElement('beforebegin', elementContainer);
   elementContainer.insertAdjacentElement('beforeend', elementKeyboard);
 
   lang.forEach((arr) => {
@@ -46,7 +95,7 @@ const createKeyboard = (lang) => {
   });
 };
 
-createKeyboard(KEYS_ENG);
+createKeyboard(KEYS_ENG.lowerCase);
 
 // const body = document.querySelector('body');
 // const keyboard = document.querySelector('.keyboard');
